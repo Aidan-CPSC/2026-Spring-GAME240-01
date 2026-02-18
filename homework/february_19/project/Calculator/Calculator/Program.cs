c1cb7aa0d7d2406ea25ed8ecdfa3faae50b34e37
Aidan Stewart <ahstewart06@icloud.com>
2026-02-18 12:32:29 -0800

    created calculator program

---
+++ homework/february_19/project/Calculator/Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("This calculator can perform 4 operations: addition, subtraction, multiplication, and division.");
+Console.WriteLine("What operation would you like to perform?");
+string operation = Console.ReadLine();
+if (operation == "addition")
+{
+    Console.WriteLine("What is your first number?");
+    double firstNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("What is your second number?");
+    double secondNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine(firstNumber + " + " + secondNumber + " is " + (firstNumber + secondNumber));
+}
+else if (operation == "subtraction")
+{
+    Console.WriteLine("What is your first number?");
+    double firstNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("What is your second number?");
+    double secondNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine(firstNumber + " - " + secondNumber + " is " + (firstNumber - secondNumber));
+}
+else if (operation == "multiplication")
+{
+    Console.WriteLine("What is your first number?");
+    double firstNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("What is your second number?");
+    double secondNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine(firstNumber + " * " + secondNumber + " is " + (firstNumber * secondNumber));
+}
+else if (operation == "division")
+{
+    Console.WriteLine("What is your first number?");
+    double firstNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("What is your second number?");
+    double secondNumber = int.Parse(Console.ReadLine());
+    if (secondNumber == 0) Console.WriteLine("You cannot divide by zero.");
+    else Console.WriteLine(firstNumber + " / " + secondNumber + " is " + (firstNumber / secondNumber));
+}
+else
+{
+    Console.WriteLine("I do not know how to do that.");
+}
