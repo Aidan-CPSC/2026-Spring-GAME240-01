f144abdda892ef7c7d92246a24958d794e58481b
Aidan Stewart <ahstewart06@icloud.com>
2026-03-11 11:39:03 -0700

    completed Program for March 12th hw

---
+++ homework/march_9/submission/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numbers = {4, 2, 99, 100, -5};
+
+int smallest = numbers[0];
+int largest = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] < smallest)
+    {
+        smallest = numbers[i];
+    }
+
+    if (numbers[i] > largest)
+    {
+        largest = numbers[i];
+    }
+}
+
+Console.WriteLine("The largest number is " + largest + ". The smallest number is " + smallest + ".");
